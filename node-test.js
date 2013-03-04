df082ab56f3392948e0044643950217362cc2dc8
David Aasterud <daa@Davids-MacBook-Pro.local>
2013-03-04 19:31:08 +0100

    added listing of the uploaded images

--- node-test.js
+++ node-test.js
@@ -4,8 +4,22 @@ var spawn = require('child_process').spawn;
 var AWS = require('aws-sdk');
 var fs = require("fs");
 var querystring = require('querystring');
+//var sys = require('util');
+//var counter = 0;
 
 http.createServer(function (req, res) {
+  //sys.puts('Counter ' + counter + " from " + req.url);
+
+  if (req.url === '/favicon.ico') {
+    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    res.end();
+    console.log('favicon requested');
+    return;
+  }
+
+  AWS.config.loadFromPath('./config.json');
+  // Set region for future requests.
+  AWS.config.update({region: 'us-east-1'});
   req.on('data', function(data) {
     var queryData = JSON.parse(data.toString('utf-8'));
     //console.log(data.toString('utf-8'));
@@ -17,21 +31,29 @@ http.createServer(function (req, res) {
     }
   });
   
-  res.writeHead(200, {"Content-Type": "application/json"});
-  res.write(
-    JSON.stringify({ 
-      status: "ok"
-    })
-  );
-  res.end();
-  
+  var keyCollection = [];
+  var s3 = new AWS.S3();
+  s3.client.listObjects({ Bucket : 'screenshotter-bucket' }, function(err, data){
+    if (err) throw err;
+    for(c in data.Contents) {
+      console.log(data.Contents[c].Key);
+      keyCollection.push({"fileName": data.Contents[c].Key});
+    }
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(
+      JSON.stringify({ 
+        images: keyCollection
+      })
+    );
+    res.end();
+  }); 
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
-var startScreenshotProcess = function(url_to_grab, image_name) {
-  if(image_name !== undefined && url_to_grab !== undefined) {
-    console.log("url to grab: " + url_to_grab + " image name: " + image_name);
-    screenshotter = spawn('phantomjs', ['screenshotter.js', url_to_grab, 'images/'+image_name]);
+var startScreenshotProcess = function(urlToGrab, imageName) {
+  if(imageName !== undefined && urlToGrab !== undefined) {
+    console.log("url to grab: " + urlToGrab + " image name: " + imageName);
+    screenshotter = spawn('phantomjs', ['screenshotter.js', urlToGrab, 'images/'+imageName]);
 
     screenshotter.stdout.on('data', function (data) {
       console.log('stdout: ' + data);
@@ -43,24 +65,21 @@ var startScreenshotProcess = function(url_to_grab, image_name) {
 
     screenshotter.on('exit', function (code) {
       console.log('child process exited with code ' + code);
-      uplooadToS3(image_name);
+      uplooadToS3(imageName);
     });
   }
 }
 
-var uplooadToS3 = function(image_name) {
-  AWS.config.loadFromPath('./config.json');
-  // Set region for future requests.
-  AWS.config.update({region: 'us-east-1'});
-  fs.readFile('images/'+image_name, function (err, data) {
-    var params = {Bucket: 'screenshotter-bucket', Key: image_name, Body: data};
+var uplooadToS3 = function(imageName) {
+  fs.readFile('images/'+imageName, function (err, data) {
+    var params = {Bucket: 'screenshotter-bucket', Key: imageName, Body: data};
     var s3 = new AWS.S3();
     s3.client.putObject(params, function(err, data) {
       if (err) {
         console.log(err)  
       }
       else {
-        console.log("Successfully uploaded data to screenshotter-bucket/"+image_name);
+        console.log("Successfully uploaded data to screenshotter-bucket/"+imageName);
       }
     });
   });
